250b1712f8c3b53ff12968a3ca42a75df9acf034
Guilherme <51514376+guilherme963@users.noreply.github.com>
2019-06-08 15:56:32 -0300

    George 1,1

--- Sofia.py
+++ Sofia.py
@@ -1,36 +1,39 @@
 import Configurações 
 import Recognizer
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 import serial
 config_ = Configurações.Config('default') #Default
 
-
-
 print('-' *100)
-
 nome_do_bot = str(input('Como deseja que Bot se chame: '))
-
 print('-' *100)
 
-porta = str(input('Digite a porta serial em que esta conectado seu arduino: ')).upper()
+try:
+    porta = str(input('Digite a porta serial em que esta conectado seu arduino: ')).upper()
+    ser = serial.Serial(porta)  # Abre uma porta
 
-bot = ChatBot(nome_do_bot)
-#trainer = ChatterBotCorpusTrainer(bot)
-#trainer.train([
-#  'Oi',
-#   'Olá!'
-# ])
+except:
+    porta = str(input(porta + ' não é uma porta valida, digite-a novamente: ')).upper()
+    ser = serial.Serial(porta)
+    
+    
 
-ser = serial.Serial(porta)  # Abre uma porta
+#------------------Treinamento_do_bot---------------------------------
+bot = ChatBot(nome_do_bot)          
+trainer = ListTrainer(bot)                  
+trainer.train([
+   'Olá!',
+   "Como posso ajuda-lo?"
+ ])
 
-
+#-----------------------------------------------------------------------------------
 print ("Diga Olá!: ")
 while True:
 
     
     if  config_[0] % 2 == 0:
-        print('"Ouvindo"')
+        print('~Ouvindo')
         fala = Recognizer.Fala()  # Reconhecimento de voz
         
     else: fala = str(input('Digite sua fala ')) # String de texto (modo sem voz)
