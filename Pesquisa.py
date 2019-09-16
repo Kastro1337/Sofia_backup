6d021c34c26228db3630403a65127ad11755f68d
Angelo Castro <50339824+Kastro1337@users.noreply.github.com>
2019-09-16 09:41:45 -0300

    clean-up 2
    movido para desfuncs

--- Pesquisa.py
+++
@@ -1,7 +0,0 @@
-from googlesearch import search
-import webbrowser
-
-def  pesquisa(tema_da_pesquisa):
-     for url in search(tema_da_pesquisa, stop= 1):
-          #print (url)
-          webbrowser.open(url)
